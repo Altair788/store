e3039be3e4faae9f5d58395fde700f0901e9e4f9
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-08-17 22:48:15 +0300

    add save_to_file, update def contact()

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from configs import FEEDBACKS_PATH
+from utils import write_to_file
+
 
 # Create your views here.
 def home(request):
@@ -7,4 +10,18 @@ def home(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        feedback_dict = {}
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"New message from {name} ({phone}): {message}")
+
+        feedback_dict['name'] = name
+        feedback_dict['phone'] = phone
+        feedback_dict['message'] = message
+
+        write_to_file(feedback_dict, FEEDBACKS_PATH)
+        print("Обращение записано.")
+
     return render(request, 'main/contact.html')
---
+++ utils.py
@@ -0,0 +1,32 @@
+import json
+from pathlib import Path
+from typing import Any
+
+
+def write_to_file(data: dict[str, Any], filename: Path) -> None:
+    """
+    Записывает данные в файл в формате списка словарей.
+
+    Если файл существует, функция загружает существующие данные, добавляет новый словарь
+    и перезаписывает файл с обновленным списком.
+
+    Args:
+        data (Dict[str, Any]): Сообщение от клиента, которое нужно записать.
+        filename (Path): Путь к файлу, в который будут записаны данные.
+
+    Returns:
+        None
+    """
+    if filename.exists():
+        with open(filename, 'r', encoding='utf-8') as f:
+            try:
+                existing_data = json.load(f)
+            except json.JSONDecodeError:
+                existing_data = []
+    else:
+        existing_data = []
+
+    existing_data.append(data)
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(existing_data, f, ensure_ascii=False, indent=4)
