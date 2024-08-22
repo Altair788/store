86749d620e488ab342c76f0b378c392e81e4afd7
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-08-22 18:40:56 +0300

    прописал обработку media

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home, contact
@@ -5,6 +7,6 @@ from catalog.views import home, contact
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='main/home'),
-    path('contacts/', contact, name='main/contact'),
-]
+                  path('', home, name='main/home'),
+                  path('contacts/', contact, name='main/contact'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
