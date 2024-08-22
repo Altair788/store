fa6a71981204e5e9197fd4163c2dc9d07353e940
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-08-22 17:24:44 +0300

    add db

--- config/settings.py
+++ config/settings.py
@@ -53,10 +53,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_store',
+        'USER': 'altair',
+        'PASSWORD': 'gI2onNlGdh1D',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
