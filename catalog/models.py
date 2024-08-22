7032cedf5de2a6ad6e0355dacc5943436ce4f7be
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-08-22 17:34:34 +0300

    update models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Представляет класс Категория
+    """
+    title = models.CharField(max_length=200, verbose_name='Категория')
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    """
+    Представляет класс Продукт
+    """
+    title = models.CharField(max_length=200, verbose_name='Продукт')
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
