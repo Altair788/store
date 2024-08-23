550c1e90002765122a3fc4ba3a1ad09339865ade
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-08-23 16:06:22 +0300

    custom_command completed

--- configs.py
+++ configs.py
@@ -3,3 +3,4 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent
 
 FEEDBACKS_PATH = ROOT_PATH.joinpath('data', 'feedbacks.json')
+CATALOG_DATA_PATH = ROOT_PATH.joinpath('data', 'catalog_data.json')
