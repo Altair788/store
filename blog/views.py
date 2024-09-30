de8de08f72dcf7e7f469b399edabee61017a9b70
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-10-01 01:05:40 +0300

    task 2 completed

---
+++ blog/views.py
@@ -0,0 +1,67 @@
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
+
+from blog.models import Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = (
+        'title',
+        'body',
+        'preview',
+    )
+    success_url = reverse_lazy('blog:article_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_article = form.save()
+            new_article.slug = slugify(new_article.title)
+            new_article.save()
+
+        return super().form_valid(form)
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = (
+        'title',
+        'body',
+        'preview',
+    )
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_article = form.save()
+            new_article.slug = slugify(new_article.title)
+            new_article.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:article_update', args=[self.kwargs.get('pk')])
+
+
+class ArticleListView(ListView):
+    model = Article
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    success_url = reverse_lazy('blog:article_list')
