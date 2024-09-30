af53fc2b46a6e807603e9455b5b92317a373a9dc
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-09-30 16:28:43 +0300

    task 1 completed

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import contact, products_list, product_detail
+from catalog.views import ProductListView, ProductDetailView, ContactView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", products_list, name="main/products_list"),
-    path("contacts/", contact, name="main/contact"),
-    path("products/<int:pk>/", product_detail, name="main/product_detail"),
+    path("", ProductListView.as_view(), name="catalog/product_list"),
+    path("contacts/", ContactView.as_view(), name="catalog/contact"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="catalog/product_detail"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
