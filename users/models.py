39054ad6e2e08fe961af750d8da88880ca000c57
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2024-10-13 16:37:56 +0300

    add superuser

--- users/models.py
+++ users/models.py
@@ -12,6 +12,10 @@ class User(AbstractUser):
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     country = models.CharField(max_length=100, verbose_name='страна', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    company_name = models.TextField(
+        verbose_name="Название компании", help_text="Введите название компании", **NULLABLE
+    )
+    inn = models.CharField(max_length=12, verbose_name='ИНН', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
